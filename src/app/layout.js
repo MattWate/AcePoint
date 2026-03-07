7544695cacd77441c1067945bbf1452e85ff736c
MattWate <matthew@dubyahinnovation.com>
2026-03-07 20:48:52 +0200

    Integrate NavBar into RootLayout component
    Added NavBar component to RootLayout and adjusted body padding.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import NavBar from '@/components/ui/NavBar'
 
 export const metadata = {
   title: 'AcePoint',
@@ -9,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">{children}</body>
+      <body className="bg-black text-white pb-24">
+        {children}
+        <NavBar />
+      </body>
     </html>
   )
 }
